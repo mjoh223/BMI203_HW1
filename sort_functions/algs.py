11dc5b0e9cf310c47d7a2b0ced0bc6d4a70a65e0
Matt Johnson <mjoh223@lsu.edu>
2019-01-25 12:21:49 -0800

    added plotting code

--- sort_functions/algs.py
+++ sort_functions/algs.py
@@ -7,51 +7,106 @@ def pointless_sort(x):
     """
     return np.array([1,2,3])
 
-def bubblesort(x):
+def bubblesort(x): #1 conditional and 2 assignments
     """
     Describe how you are sorting `x`
     """
+    c = 0
+    a = 0
     for passnum in range(len(x)-1,0,-1): #count backwards from size of list
         for i in range(passnum):
-            if x[i]>x[i+1]:#conditional
-                x[i], x[i+1] = x[i+1], x[i] #two assignment
-    return x
+            if x[i]>x[i+1]: #conditional
+                c += 1
+                a += 2
+                x[i], x[i+1] = x[i+1], x[i] #two assignments
+    return [x,c,a]
 
-def quicksort(x):
-    quickSortHelper(x,0,len(x)-1)
-    return x
+def quicksort(x): #14 assignments and 5 conditionals
+    c = 0
+    a = 0
+    ret = quickSortHelper(x,0,len(x)-1,c,a)
+    return ret
 
-def quickSortHelper(x, first, last):
-    if first < last:
+def quickSortHelper(x, first, last,c,a):
+    if first < last: #conditional
+        c += 1
+        a += 1
+        splitpoint = partition(x, first, last, c, a) #assignment
 
-        splitpoint = partition(x, first, last)
+        sorted_list, c, a = quickSortHelper(x, first, splitpoint-1, c, a)
+        sorted_list, c, a = quickSortHelper(x, splitpoint+1, last, c, a)
+    return [x,c,a]
 
-        quickSortHelper(x, first, splitpoint-1)
-        quickSortHelper(x, splitpoint+1, last)
+def partition(x, first, last, a, c):
+    a += 1
+    pivotvalue = x[first] #assignment
+    a += 1
+    leftmark = first+1 #assignment
+    a += 1
+    rightmark = last #assignment
 
-def partition(x, first, last):
-    pivotvalue = x[first]
-
-    leftmark = first+1
-    rightmark = last
-
-    done = False
+    done = False #assignment
+    c += 1
     while not done:
 
-        while leftmark <= rightmark and x[leftmark] <= pivotvalue:
-            leftmark = leftmark + 1
+        while leftmark <= rightmark and x[leftmark] <= pivotvalue: #two conditionals
+            c += 2
+            a += 1
+            leftmark = leftmark + 1 #assignment
 
-        while x[rightmark] >= pivotvalue and rightmark >= leftmark:
-            rightmark = rightmark -1
+        while x[rightmark] >= pivotvalue and rightmark >= leftmark: #two conditionals
+            c += 2
+            a += 1
+            rightmark = rightmark -1 #assignment
 
-        if rightmark < leftmark:
-            done = True
+        if rightmark < leftmark: #conditional
+            c += 1
+            a += 1
+            done = True #assignment
         else:
-            temp = x[leftmark]
-            x[leftmark] = x[rightmark]
-            x[rightmark] = temp
-
-    temp = x[first]
-    x[first] = x[rightmark]
-    x[rightmark] = temp
+            a += 3
+            temp = x[leftmark] #assignment
+            x[leftmark] = x[rightmark] #assignment
+            x[rightmark] = temp #assignment
+    a += 3
+    temp = x[first] #assignment
+    x[first] = x[rightmark] #assignment
+    x[rightmark] = temp #assignment
     return rightmark
+
+quicksort(np.random.randint(20, size=20))
+quicksort(np.random.randint(20, size=200))
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+timed = []
+for i in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]:
+    x = np.random.randint(i, size=i)
+    wrappedb = wrapper(bubblesort, x)
+    tim = timeit.timeit(wrappedb, number=100)
+    timed.append([i,"bubblesort", tim])
+for i in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]:
+    x = np.random.randint(i, size=i)
+    wrappedq = wrapper(quicksort, x)
+    timq = timeit.timeit(wrappedq, number=100)
+    timed.append([i,"quicksort", timq])
+
+timed
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+timed = pd.DataFrame(timed)
+timed.columns = ["time", "algorithm", "speed"]
+sns.lmplot(x="time", y="speed", col="algorithm",order=2,data=timed, ci=False)
+np.polyfit(x=timed.iloc[:,0], y=timed.iloc[:,2], deg=3)
+
+pd.DataFrame(timed)
+
+df = sns.load_dataset("anscombe")
+df
