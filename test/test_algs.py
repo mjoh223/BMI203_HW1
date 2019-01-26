e0fcc25b845c685602f54cbd6e7502c38cd3216e
Matt Johnson <mjoh223@lsu.edu>
2019-01-25 17:35:52 -0800

    added more modules

--- test/test_algs.py
+++ test/test_algs.py
@@ -5,8 +5,7 @@ import timeit
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy import stats
+
 
 def test_pointless_sort():
     # generate random vector of length 10
